38e8e7fc783a3585b98a596380c13d4c596794fd
Matt Blair <blair1618@gmail.com>
2016-08-04 19:38:23 -0400

    WIP iOS build system refactor

---
+++ ios/tangram_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tangram_demo
+//
+//  Created by Matt Blair on 8/4/16.
+//  Copyright © 2016 mapzen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
